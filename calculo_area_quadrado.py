d34397e654d4d8c37fe0fb64122b854527358206
Adriano Bernardo Cerutti <adrianocerutti@gmail.com>
2017-09-16 23:57:20 -0300

    Create calculo_area_quadrado.py

---
+++ calculo_area_quadrado.py
@@ -0,0 +1,10 @@
+#Imprime uma mensagem na tela
+print("Calcular a Área de um Quadrado")
+#Recupera o valor digitado pelo usuário e armazena em uma variável
+lado = float(input("Digite um valor: "))
+#Realiza o cálcula da área de em quadrado (A = L x L)
+area = lado * lado
+#Imprime o resultado na tela
+print("Área = ",area)
+#Calcula o dobro do valor da área e exibe uma mensagem na tela com resultado
+print("O dobro da área é", area * 2)
